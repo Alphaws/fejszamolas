23ad4b9264da1183b2dc4711ed40d4e2436a1283
Alphaws <alphaws@gmail.com>
2024-11-14 10:46:39 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'fejszamolas',
+  webDir: 'www'
+};
+
+export default config;
