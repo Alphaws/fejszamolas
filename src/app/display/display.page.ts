c4d3a96079238dc5e72aa25f915fa0d51494e2b7
Alphaws <alphaws@gmail.com>
2024-11-15 02:27:03 +0100

    numbers

--- src/app/display/display.page.ts
+++ src/app/display/display.page.ts
@@ -1,21 +1,74 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { ActivatedRoute, Router } from "@angular/router";
+import { IonicModule } from "@ionic/angular";
 
 @Component({
   selector: 'app-display',
   templateUrl: './display.page.html',
   styleUrls: ['./display.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [CommonModule, FormsModule, IonicModule]
 })
 export class DisplayPage implements OnInit {
+
+  route: any = inject(ActivatedRoute);
+  router: any = inject(Router);
+
+  ready: boolean = false;
+
+  numberCount!: number;
+  displayTime!: number;
+  digitLength!: number;
+  numbers: number[] = [];
+  currentIndex: number = 0;
+  currentNumber!: number | null;
+  userAnswer: string = '';
 
   constructor() { }
 
   ngOnInit() {
-    console.log('init');
+    this.ready = false;
+    this.currentIndex = 0;
+    this.numbers = [];
+    this.route.queryParams.subscribe((params: any) => {
+      this.numberCount = +params['numberCount'];
+      this.displayTime = +params['displayTime'];
+      this.digitLength = +params['digitLength'];
+      this.generateNumbers();
+      this.showNextNumber();
+    });
   }
 
+  generateNumbers() {
+    for (let i = 0; i < this.numberCount; i++) {
+      const number = Math.floor(Math.random() * Math.pow(10, this.digitLength));
+      this.numbers.push(number);
+    }
+  }
+
+  showNextNumber() {
+    if (this.currentIndex < this.numbers.length) {
+      this.currentNumber = this.numbers[this.currentIndex];
+      setTimeout(() => {
+        this.currentNumber = null;
+        setTimeout(() => {
+          this.currentIndex++;
+          this.showNextNumber();
+        }, 500);
+      }, this.displayTime * 1000);
+    } else {
+      this.ready = true;
+    }
+  }
+
+  back() {
+    this.router.navigate(['/']).then();
+  }
+
+  restart() {
+    this.ngOnInit();
+
+  }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,45 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, inject } from '@angular/core';
+import { IonicModule, PopoverController } from "@ionic/angular";
+import { FormsModule } from "@angular/forms";
+import { Router } from "@angular/router";
+import { UserMenuComponent } from "../user-menu/user-menu.component";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    FormsModule,
+    IonicModule,
+  ],
 })
 export class HomePage {
+
+  router: Router = inject(Router);
+  popoverController: PopoverController = inject(PopoverController);
+  numberCount: number = 5;
+  displayTime: number = 2;
+  digitLength: number = 2;
+
   constructor() {}
+
+  startGame() {
+    this.router.navigate(['/display'], {
+      queryParams: {
+        numberCount: this.numberCount,
+        displayTime: this.displayTime,
+        digitLength: this.digitLength,
+      },
+    });
+  }
+
+  async openUserMenu($event: MouseEvent) {
+    const popover = await this.popoverController.create({
+      component: UserMenuComponent,
+      event: $event,
+      translucent: true,
+    });
+    await popover.present();
+  }
 }
